0ac4cde5317760df2a359899fb15630b60e0df89
theogonzo <theo.gonzo@hotmail.fr>
2022-05-12 08:58:19 +0200

    BDD
    UML + Création BDD + Jeu de Données

---
+++ CreatBDD.sql
@@ -0,0 +1,65 @@
+------------------------
+--CréationBDD
+------------------------
+
+create table Client (
+    num_membre integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom varchar(128) NOT NULL,
+    prenom varchar(128) NOT NULL,
+    adresse_postale varchar(128) NOT NULL,
+    adresse_mail varchar(128) NOT NULL,
+    num_tel varchar(20) NOT NULL,
+    contrat boolean,
+    motif_contrat ENUM('Professionel', 'Personnel') 
+
+);
+
+create table Borne (
+    id_borne integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    etat ENUM('disponible', 'indisponible', 'occupée', 'réservée') 
+
+);
+
+create table Parametre (
+    nom_param varchar(128) PRIMARY KEY NOT NULL,
+    valeur varchar(128)
+    
+);
+
+create table Reservation (
+    num_res integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date_deb datetime NOT NULL,
+    date_fin datetime NOT NULL,
+    delai_retard integer,
+    num_membre integer NOT NULL,
+    id_borne integer NOT NULL,
+
+    FOREIGN KEY (num_membre) REFERENCES Client(num_membre),
+    FOREIGN KEY (id_borne) REFERENCES Borne(id_borne)
+);
+
+
+create table Transaction (
+    id_transaction integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    montant_charge DECIMAL(10,2),
+    montant_res DECIMAL(10,2),
+    montant_penalite DECIMAL(10,2),
+    num_membre integer NOT NULL,
+    num_res integer NOT NULL,
+
+    FOREIGN KEY (num_membre) REFERENCES Client(num_membre),
+    FOREIGN KEY (num_res) REFERENCES Reservation(num_res)
+);
+
+
+create table Vehicule (
+    immatriculation varchar(128) PRIMARY KEY NOT NULL,
+    marque varchar(128) NOT NULL,
+    modele varchar(128) NOT NULL,
+    id_borne integer,
+    num_membre integer NOT NULL,
+
+    FOREIGN KEY (num_membre) REFERENCES Client(num_membre),
+    FOREIGN KEY (id_borne) REFERENCES Borne(id_borne)
+);
+
---
+++ JeuDonnées.sql
@@ -0,0 +1,42 @@
+------------------------
+--JeuDonnées
+------------------------
+
+insert into Borne (id_borne, etat) values
+        (1, 'occupée'),
+        (2, 'réservée'),
+        (3, 'disponible'),
+        (4, 'indisponible');
+commit;
+
+insert into Parametre (nom_param, valeur)
+values  ('periode_att', 15),
+        ('nb_limit_prolong', 3);
+commit;
+
+insert into Client (num_membre, nom, prenom, adresse_postale, adresse_mail, num_tel, contrat, motif_contrat) values
+        (1, 'Gonzalez', 'Théo', '12 Rue Stanislas 54000 Nancy', 'theo.g@gl.com', '0601010101', true, 'Professionel'),
+        (2, 'Repetti', 'Alexandra', '14 Rue Stanislas 54000 Nancy', 'alex.r@gl.com', '0602020202', true, 'Personnel'),
+        (3, 'Mathieu', 'Jean', '16 Rue Stanislas 54000 Nancy', 'jean.m@gl.com', '0603030303', false, null),
+        (4, 'Pirus', 'Marine', '18 Rue Stanislas 54000 Nancy', 'marine.p@gl.com', '0604040404', true, 'Personnel');
+commit;
+
+insert into Reservation (num_res, date_deb, date_fin, delai_retard, num_membre, id_borne) values
+        (1, '2022-05-08 11:30:00', '2022-05-08 12:30:00', null, 2, 3),
+        (2, '2022-05-11 11:30:00', '2022-05-11 12:30:00', null, 2, 3),
+        (3, '2022-05-14 11:30:00', '2022-05-14 12:30:00', null, 2, 3),
+        (4, '2022-05-15 11:30:00', '2022-05-15 12:30:00', null, 2, 3);
+commit;
+
+insert into Transaction (id_transaction, montant_charge, montant_res, montant_penalite, num_membre, num_res)
+values  (1, 10.3 , 13.4, 11.3, 1, 2),
+        (2, 11.3 , 10.4, 15.3, 2, 1),
+        (3, 15.3 , 16.4, 13.3, 3, 3);
+commit;
+
+insert into Vehicule (immatriculation, marque, modele, id_borne, num_membre)
+values  ('AA-111-BB', 'Tesla', '3', 3, 1),
+        ('AB-112-BC', 'Citroën', 'DS4', 2, 2),
+        ('AC-113-BD', 'BMW', 'i7', 3, 1),
+        ('AD-114-BE', 'Toyota', 'bZ4x', null, 3);
+commit;
