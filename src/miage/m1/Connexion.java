aba168c0c451168b1b615b340a10dd16db43a172
AlexRepetti <a-rep@live.fr>
2022-05-12 09:11:01 +0200

    test recup theo

--- src/miage/m1/Connexion.java
+++ src/miage/m1/Connexion.java
@@ -6,22 +6,32 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Connexion{
-    public static void main(String[] args) {
-        try
-        {
+	private static Connection con ;
+	
+	
+    public static void openConnexion() {
+        try{
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/GenieLogiciel","root","");
-            Statement stmt=con.createStatement();  
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Borne");
-            while (rs.next()) {
-            	System.out.println(rs.getInt("id_borne"));
-            }
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/GenieLogiciel","root","");
             
             System.out.println("Connected");  
         }
-        catch(Exception e)
-        {
+        catch(Exception e){
             System.out.println(e);
         }
-    }  
+    }
+    
+    public void fetchUser() {
+    	try {
+    		Statement stmt=con.createStatement();  
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Client WHERE ");
+            while (rs.next()) {
+            	System.out.println(rs.getInt("id_borne"));
+            }
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    }
+    
+    
 }
