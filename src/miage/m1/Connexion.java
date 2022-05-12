a5d1504b9c1aa00f71bd483b945860d282ba8414
AlexRepetti <a-rep@live.fr>
2022-05-12 16:26:08 +0200

    connexion et verif

--- src/miage/m1/Connexion.java
+++ src/miage/m1/Connexion.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Connexion{
 	private static Connection con ;
 	
-	
+	//connection a la BDD
     public static void openConnexion() {
         try{
             Class.forName("com.mysql.jdbc.Driver");
@@ -21,17 +23,44 @@ public class Connexion{
         }
     }
     
-    public void fetchUser() {
+    
+    //rechercher User avec son numero de membre 
+    public void fetchUser(String num) {
     	try {
     		Statement stmt=con.createStatement();  
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Client WHERE ");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Client WHERE num_membre="+num);
             while (rs.next()) {
-            	System.out.println(rs.getInt("id_borne"));
+            	System.out.println(rs.getString("nom"));
             }
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println(e);
 		}
+    }
+    
+    //ajout d'un nouvel utilisateur 
+    public void addUser(String nom, String prenom, String adresse, String email, int num, Boolean contrat, String motif) {
+    	try {
+    		Statement stmt=con.createStatement();  
+            int rs = stmt.executeUpdate("insert into Client (nom, prenom, adresse_postale, adresse_mail, num_tel, contrat, motif_contrat) values ('"
+            		+nom+"','"+prenom+"','"+adresse+"','"+email+"','"+num+"',"+contrat+","+motif+""
+            				+ ")");
+            
+            System.out.println("Creation OK");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+    }
+    
+    //verification de l'adresse email
+    public boolean verifMail(String mail) {  
+        String regx = "^[A-Za-z0-9+_.-]+@(.+)$";  
+        Pattern pattern = Pattern.compile(regx);
+        Matcher matcher = pattern.matcher(mail);
+        return matcher.matches();
     }
     
     
 }
+
+
+
--- src/miage/m1/Main.java
+++ src/miage/m1/Main.java
@@ -1,11 +1,39 @@
+
 package miage.m1;
+
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Connexion connexion =new Connexion(); 
-		connexion.openConnexion();
-
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Connexion connexion =new Connexion(); 
+        connexion.openConnexion();
+        
+        /*
+        System.out.println("Création de compte : Veuillez entrez votre mail :");
+        String mail =sc.nextLine();
+        if (connexion.verifMail(mail)==true){   
+            System.out.println(" Veuillez entrez votre nom :");
+            String nom = sc.nextLine();
+            System.out.println("Veuillez entrez votre prenom :");
+            String prenom =sc.nextLine();
+            System.out.println("Veuillez entrez votre adresse :");
+            String adresse =sc.nextLine();
+            System.out.println("Veuillez entrez votre numéro de téléphone :");
+            int tel =sc.nextInt();
+            connexion.addUser(nom,prenom,adresse,mail,tel, false, null);
+        }
+        else {
+            System.out.println("adresse non valide");
+        }
+        */
+        //test recherche user 
+        connexion.fetchUser("1");
+        //test ajout user 
+        //connexion.addUser("dede", "dede", "nancy", "ok", 02, false, null);
+        
+    }
 
 }
+
