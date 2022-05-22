9ff764d738a1dc58d2459a9a8dcf88bcae36eedc
Kyouis <45875365+Kyouis@users.noreply.github.com>
2022-05-22 20:41:19 +0200

    prolongation

--- src/miage/m1/Connexion.java
+++ src/miage/m1/Connexion.java
@@ -1,12 +1,15 @@
 package miage.m1;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Connexion{
-	private static Connection con ;
+    private static Connection con ;
     static Scanner sc = new Scanner(System.in);
 
 
@@ -14,42 +17,42 @@ public class Connexion{
     public static void openConnexion() {
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8889/GenieLogiciel","root","root");
-            
-            System.out.println("Connected");  
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/GenieLogiciel","root","");
+
+            System.out.println("Connected");
         }
         catch(Exception e){
             System.out.println("Non connecté");
             System.out.println(e);
         }
     }
-    
-    
+
+
     //rechercher User avec son numero de membre 
     public void fetchUser(String num) {
-    	try {
-    		Statement stmt=con.createStatement();  
+        try {
+            Statement stmt=con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM Client WHERE num_membre="+num);
             while (rs.next()) {
-            	System.out.println(rs.getString("nom"));
+                System.out.println(rs.getString("nom"));
             }
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    
+
     //ajout d'un nouvel utilisateur 
     public void addUser(String nom, String prenom, String adresse, String email, int num, Boolean contrat, String motif) {
-    	try {
-    		Statement stmt=con.createStatement();  
+        try {
+            Statement stmt=con.createStatement();
             int rs = stmt.executeUpdate("insert into Client (nom, prenom, adresse_postale, adresse_mail, num_tel, contrat, motif_contrat) values ('"
-            		+nom+"','"+prenom+"','"+adresse+"','"+email+"','"+num+"',"+contrat+","+motif+""
-            				+ ")");
-            
+                    +nom+"','"+prenom+"','"+adresse+"','"+email+"','"+num+"',"+contrat+","+motif+""
+                    + ")");
+
             System.out.println("Creation OK");
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
     //vérification borne dispo
     public int firstBorneDispo() {
@@ -83,17 +86,36 @@ public class Connexion{
             Statement s = con.createStatement();
             String q = "INSERT INTO RESERVATION (date_deb, date_fin, num_membre, id_borne) VALUES ('"+dateDebut+"','"+dateFin+"','"+user+"','"+numborne+"')";
             System.out.println(q);
-             int t = s.executeUpdate(q);
+            int t = s.executeUpdate(q);
             System.out.println("Reservation ok");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
     }
-    
+
+    public boolean checkProlongationPossible(String numBorne,String datefin, String dureeProl) {
+        boolean res = true;
+        try {
+            Date datefinparse= (Date) new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(datefin);
+
+            Statement s = con.createStatement();
+            String q = "SELECT * FROM RESERVATION WHERE id_borne="+numBorne;
+            ResultSet r = s.executeQuery(q);
+            while (r.next()) {
+                if (r.getDate("date_fin").after( datefinparse) && r.getDate("date_fin").before(new Date(datefinparse.getTime()+((long) Integer.parseInt(dureeProl) *60*1000)))) {
+                    res = false;
+                }
+            }
+        } catch (SQLException | ParseException throwables) {
+            throwables.printStackTrace();
+        }
+        return res;
+    }
+
     //verification de l'adresse email
-    public boolean verifMail(String mail) {  
-        String regx = "^[A-Za-z0-9+_.-]+@(.+)$";  
+    public boolean verifMail(String mail) {
+        String regx = "^[A-Za-z0-9+_.-]+@(.+)$";
         Pattern pattern = Pattern.compile(regx);
         Matcher matcher = pattern.matcher(mail);
         return matcher.matches();
